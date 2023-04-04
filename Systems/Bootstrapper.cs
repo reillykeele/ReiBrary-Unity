4315f37f5c68bd1a7c42907461a00f2f1871e41c
reillykeele <54957139+reillykeele@users.noreply.github.com>
2023-04-03 21:40:03 -0500

    Add Bootstrapper

---
+++ Systems/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Util.Systems
+{
+    public static class Bootstrapper
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Execute() => Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("Systems")));
+
+        // [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        // public static void Execute() => Object.DontDestroyOnLoad(Addressables.InstantiateAsync("Systems").WaitForCompletion);
+    }
+}
