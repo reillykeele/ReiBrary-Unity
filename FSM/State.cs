4e888aee063324bc57d8a061e1c1007783072013
Reilly Keele <reillykeele@gmail.com>
2023-04-11 21:09:12 -0500

    Add State Machine to Util

---
+++ FSM/State.cs
@@ -0,0 +1,13 @@
+﻿namespace Util.FSM
+{
+    public abstract class State
+    {
+        // Name ?
+        // ID ?
+
+        public virtual void Enter(StateMachine stateMachine) {}
+        public virtual void Update(StateMachine stateMachine) {}
+        public virtual void FixedUpdate(StateMachine stateMachine) {}
+        public virtual void Exit(StateMachine stateMachine) {}
+    }
+}
---
+++ FSM/StateMachine.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Util.FSM
+{
+    public class StateMachine : MonoBehaviour
+    {
+        [SerializeField] private bool _useUpdate = true;
+        [SerializeField] private bool _useFixedUpdate = false;
+
+        private State _currentState;
+
+        public State GetCurrentState => _currentState;
+
+        public void SetCurrentState(State nextState)
+        {
+            if (_currentState == nextState) return;
+
+            _currentState?.Exit(this);
+
+            _currentState = nextState;
+
+            _currentState?.Enter(this);
+        }
+
+        public void Update()
+        {
+            if (_useUpdate)
+                _currentState?.Update(this);
+        }
+
+        public void FixedUpdate()
+        {
+            if (_useFixedUpdate)
+                _currentState?.FixedUpdate(this);
+        }
+
+    }
+}
