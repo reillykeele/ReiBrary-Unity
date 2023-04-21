60dc3080d386044e71097e68727939607daeb173
reillykeele <54957139+reillykeele@users.noreply.github.com>
2023-04-20 23:42:43 -0500

    Add Menu Nav IMenuInputReader Events

--- Input/IMenuInputReader.cs
+++ Input/IMenuInputReader.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine.Events;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace Util.Input
 {
     public interface IMenuInputReader
     {
+        public event UnityAction<Vector2> MenuNavigateEvent;
+        public event UnityAction MenuNavigateCancelledEvent;
         public event UnityAction MenuUnpauseEvent;
         public event UnityAction MenuAcceptButtonEvent;
         public event UnityAction MenuCancelButtonEvent;
