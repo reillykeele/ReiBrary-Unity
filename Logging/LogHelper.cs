083d4df36386ec305a7a67f2522f94283946610b
reillykeele <54957139+reillykeele@users.noreply.github.com>
2024-07-01 13:32:21 -0400

    Add LogHelper

---
+++ Logging/LogHelper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ReiBrary.Logging
+{
+    public static class LogHelper
+    {
+        public static void LogMissingComponent(MonoBehaviour caller, string component)
+        {
+            Debug.LogError($"{caller.gameObject.name}'s {caller.GetType().Name}: Missing component \"{component}\"");
+        }
+    }
+}
